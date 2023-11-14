4c39d667fef205f6430f69fad757c7b12e9817be
Vincent Roduit <101014161+roduit@users.noreply.github.com>
2023-11-14 15:38:04 +0100

    bug to fix

--- code/create_all_users.py
+++ code/create_all_users.py
@@ -11,7 +11,6 @@ from copy import deepcopy
 import pandas as pd
 
 def create_all_users(df_advocate_users, df_matched_beer_users, df_rate_beer_users,debug=False):
-
     #Create a copy of the dataframes
     advocate = deepcopy(df_advocate_users)
     matched = deepcopy(df_matched_beer_users)
@@ -37,26 +36,24 @@ def create_all_users(df_advocate_users, df_matched_beer_users, df_rate_beer_user
         matched, how='left', 
         on=index_merge,
     )
-    #verify that all the users are matched
-    if(debug):
-        df_dataset_users.shape[0] - df_dataset_users.rb.isna().sum() == matched.shape[0]
-
+    drop_column = ['nbr_ratings','nbr_reviews','ba.6']
+    df_dataset_users.drop(columns = drop_column, inplace = True, axis=0)
     ########################################################
                 # -*-  Merging with Rate Beer -*-
     ########################################################
     #Format column name to perform merge on Rate Beer
-    df_dataset_users = df_dataset_users.rename(columns={'nbr_ratings': 'ba_nbr_ratings', 'nbr_reviews': 'ba_nbr_reviews', 'user_id': 'ba_user_id', 'user_name': 'ba_user_name', 'joined': 'ba_joined', 'location': 'ba_location'})
+    df_dataset_users = df_dataset_users.rename(columns={'user_id': 'ba_user_id', 'user_name': 'ba_user_name', 'joined': 'ba_joined', 'location': 'ba_location'})
     df_dataset_users = df_dataset_users.rename(columns={'rb':'joined','rb.1':'location','rb.2':'nbr_ratings','rb.3':'user_id','rb.4':'user_name','rb.5':'user_name_lower'})
-    index_merge = ['joined', 'location', 'nbr_ratings', 'user_id', 'user_name']
+    df_dataset_users.drop(columns='user_name_lower', inplace = True, axis=0)
+    index_merge = ['location', 'nbr_ratings', 'user_id', 'user_name']
     #Convert user_id type for merging
     df_dataset_users.user_id = df_dataset_users.user_id.astype(float)
     rate_beer.user_id = rate_beer.user_id.astype(float)
     #Merging with Rate Beer
     df_dataset_users = df_dataset_users.merge(rate_beer, how='outer', on=index_merge)
-    #Keep only columns of interest
-    df_dataset_users = df_dataset_users[['ba_user_id','location','joined','user_id','user_name']]
-    if(debug):
-        #verify the result
-        df_dataset_users.shape[0] == df_advocate_users.shape[0] + rate_beer.shape[0] - matched.shape[0]
-    df_dataset_users.rename(columns={'user_id':'rb_user_id','user_name':'rb_user_name'}, inplace=True)
+    df_dataset_users['joined'] = df_dataset_users['ba_joined'].fillna(df_dataset_users['joined_y'])
+    df_dataset_users['user_id'] = df_dataset_users['ba_user_id'].fillna(df_dataset_users['user_id'])
+    df_dataset_users['location'] = df_dataset_users['ba_location'].fillna(df_dataset_users['location'])
+    df_dataset_users.rename(columns={'user_name':'rb_user_name'}, inplace=True)
+    df_dataset_users = deepcopy(df_dataset_users[['user_id','ba_user_name','rb_user_name','joined','location']])
     return df_dataset_users
