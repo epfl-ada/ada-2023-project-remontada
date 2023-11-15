43e05d9554fd247c85999974cfd0c756b6a89f8b
Alexi Semiz <alexi.semiz@epfl.ch>
2023-11-15 14:17:27 +0100

    fixes and start analysis

--- code/compute_experts.py
+++ code/compute_experts.py
@@ -20,16 +20,17 @@ def normalized_score(col):
 def is_expert(score, threshold=0.4):
     return score >= threshold
 
-def compute_experts_table(df_ratings):
-    df_ratings_stat_pivot = df_ratings.pivot(index='user_name', columns='year', values='nb_ratings')
+def compute_experts_table(df_ratings,threshold=0.4):
+    df_user_ratings_per_year = df_ratings.groupby(['user_id', 'year']).size().reset_index(name='nb_ratings')
+    df_ratings_stat_pivot = df_user_ratings_per_year.pivot(index='user_id', columns='year', values='nb_ratings')
     df_ratings_stat_pivot.fillna(0, inplace=True)
     df_ratings_stat_pivot_score = df_ratings_stat_pivot.apply(compute_expert_score, axis=1)
     df_ratings_stat_pivot_score_norm = df_ratings_stat_pivot_score.apply(normalized_score, axis=0)
-    df_ratings_stat_pivot_expert = is_expert(df_ratings_stat_pivot_score_norm)
+    df_ratings_stat_pivot_expert = is_expert(df_ratings_stat_pivot_score_norm,threshold)
     return df_ratings_stat_pivot_expert
 
 def filter_year_and_add_is_expert(df,YEAR,experts_table):
     experts_year=experts_table[experts_table[YEAR]==True].index
     df_this_year=df[df["year"]==YEAR]
     df_this_year["is_expert"] = df_this_year["user_id"].isin(experts_year).astype(int)
-    return df,experts_year
+    return df_this_year,experts_year
