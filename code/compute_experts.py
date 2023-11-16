cdb9a8ee89917acb0cde2a926b6d454765f84a1b
alexisemizsd <alexi@sportsdynamics.eu>
2023-11-16 11:24:34 +0100

    modify computation of experts

--- code/compute_experts.py
+++ code/compute_experts.py
@@ -2,13 +2,11 @@ import numpy as np
 import pandas as pd
 
 def compute_expert_score(row,weight_y=2,weight_y_1=0.5,weight_y_2=0.25,weight_y_3=0.1):
-    min_year = int(row.index[0])
-    max_year = int(row.index[-1])
-    for j in range(min_year + 3, max_year + 1):
-        try:
-            row[j] = weight_y * row[j] + weight_y_1 * row[j-1] + weight_y_2 * row[j-2] + weight_y_3 * row[j-3]
-        except:
-            continue
+    row_copy=row.copy()
+    average_norm=weight_y+weight_y_1+weight_y_2+weight_y_3
+    for j in range(1996 + 3, 2017 + 1):
+        row[j] = (weight_y * row_copy[j] + weight_y_1 * row_copy[j-1] + weight_y_2 * row_copy[j-2] + weight_y_3 * row_copy[j-3])/average_norm
+        
     return row
 
 def normalized_score(col):
@@ -21,13 +19,16 @@ def normalized_score(col):
 def is_expert(score, threshold=0.4):
     return score >= threshold
 
-def compute_experts_table(df_ratings,threshold=0.4):
+def compute_experts_table(df_ratings,quantile_score_expert=0.995):
     df_user_ratings_per_year = df_ratings.groupby(['user_id', 'year']).size().reset_index(name='nb_ratings')
     df_ratings_stat_pivot = df_user_ratings_per_year.pivot(index='user_id', columns='year', values='nb_ratings')
     df_ratings_stat_pivot.fillna(0, inplace=True)
+    df_ratings_stat_pivot[1997]=np.zeros(df_ratings_stat_pivot.shape[0])
+    df_ratings_stat_pivot = df_ratings_stat_pivot.reindex(columns=[df_ratings_stat_pivot.columns[0]] + [1997] + list(df_ratings_stat_pivot.columns[1:-1]))
     df_ratings_stat_pivot_score = df_ratings_stat_pivot.apply(compute_expert_score, axis=1)
-    df_ratings_stat_pivot_score_norm = df_ratings_stat_pivot_score.apply(normalized_score, axis=0)
-    df_ratings_stat_pivot_expert = is_expert(df_ratings_stat_pivot_score_norm, threshold=0.4)
+    print("ok")
+    df_ratings_stat_pivot_expert = df_ratings_stat_pivot_score.gt(df_ratings_stat_pivot_score[df_ratings_stat_pivot_score.gt(0)].quantile(quantile_score_expert))
+
     df_reset = df_ratings_stat_pivot_expert.reset_index()
     df_melted = pd.melt(df_reset, id_vars=['user_id'], var_name='year', value_name='is_expert')
     df_ratings_stat_expert = df_user_ratings_per_year.merge(df_melted)
